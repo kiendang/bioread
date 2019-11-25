0d6a2ac5a86d67ec8da1460f239ce8a77e0015fb
Kien Dang <mail@kien.ai>
2019-11-25 12:37:30 +0800

    Edit w2v2glove.py

--- src/scripts/w2v2glove.py
+++ src/scripts/w2v2glove.py
@@ -5,6 +5,5 @@ from pathlib import Path
 
 W2V_DIR = Path.home()/'data'
 
-
-model = KeyedVectors.load_word2vec_format(W2V_DIR/'pubmed2018_w2v_200D'/'pubmed2018_w2v_200D.bin', binary=True)
-model.save_word2vec_format(W2V_DIR/'pubmed2018_w2v_200D'/'pubmed2018_w2v_200D.txt', binary=False)
+model = KeyedVectors.load_word2vec_format(W2V_DIR/'pubmed2018_w2v_200D.bin', binary=True)
+model.save_word2vec_format(W2V_DIR/'pubmed2018_w2v_200D.txt', binary=False)
